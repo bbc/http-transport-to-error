ea01ba3e19b51d2d57c9bf78b577effa0df9ace2
Jamie Reid Bower <jamie.reidbower@bbc.co.uk>
2023-09-28 16:06:48 +0100

    upgrade http-transport, fixes linting warnings, removes coverage badge

--- test/toError.js
+++ test/toError.js
@@ -12,55 +12,44 @@ const url = `${host}${path}`;
 const api = nock(host);
 const textResponseBody = 'inimicum tuum';
 
-async function assertFailure(promise, message) {
-  try {
-    await promise;
-    assert.ok(false, 'Promise should have failed')
-  } catch (e) {
-    assert.ok(e);
-    if (message) {
-      assert.equal(e.message, message);
-    }
-  }
-}
-
 async function assertErrorResponse(code) {
-  nock.cleanAll();
   api.get(path).reply(code);
 
   const client = HttpTransport.createBuilder()
     .use(toError())
     .createClient();
 
-  const response = client
-    .get(url)
-    .asBody();
-
-  return await assertFailure(response, `Received HTTP code ${code} for GET http://www.example.com/foo`);
+  try {
+    await client
+      .get(url)
+      .asResponse();
+    assert.ok(false, 'Promise should have failed');
+  } catch (e) {
+    assert.ok(e);
+    assert.equal(e.message, `Received HTTP code ${code} for GET http://www.example.com/foo`);
+  }
 }
 
 describe('toError', () => {
   beforeEach(() => {
     nock.disableNetConnect();
     nock.cleanAll();
-    api.get(path).reply(200, textResponseBody);
   });
 
-  it('returns an error for 4XX responses', () => {
-    return assertErrorResponse(400);
+  it('returns an error for 4XX responses', async () => {
+    await assertErrorResponse(400);
   });
 
-  it('returns an error for 5XX responses', () => {
-    return assertErrorResponse(500);
+  it('returns an error for 5XX responses', async () => {
+    await assertErrorResponse(500);
   });
 
   it('does not convert to error for 2XX responses', async () => {
-    nock.cleanAll();
     api.get(path).reply(200, textResponseBody);
 
     const client = HttpTransport.createBuilder()
       .use(toError())
-      .createClient()
+      .createClient();
 
     const res = await client
       .get(url)
